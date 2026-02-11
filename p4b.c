d5e954359757775b33444ae2ffca4fc259bb0789
Mounika P <pmounika382008@gmail.com>
2026-02-11 08:22:42 +0530

    Update p4b.c

--- p4b.c
+++ p4b.c
@@ -15,4 +15,15 @@ int main() {
     *(ptr + 2) = 35; 
     printf("Modified third element: %d\n", numbers[2]);
     return 0;
+
 }
+output:Accessing elements using pointers:
+First element: 10
+Second element: 20
+All elements using a loop:
+Element 0: 10
+Element 1: 20
+Element 2: 30
+Element 3: 40
+Element 4: 50
+Modified third element: 35
